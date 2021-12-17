f7e3df26fe1932b2315395202c9d6dee3d024576
SouadTorfi <souad.torfi99@gmail.com>
2021-12-17 10:21:55 +0200

    placeholder image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
-
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
+//import Search from "./components/Search";
+//import SayHi, { SayHello } from "./components/WeatherItem";
+//import fakeWeatherData from "./fakeWeatherData.json";
+import partlycloudy from './img/weather-icons/mostlycloudy.svg'
+import clear from './img/weather-icons/clear.svg'
 
 import "./App.css";
 
@@ -21,12 +22,71 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+
+        <nav className="navbar">
+        
+          <input placeholder="london" type="search"/>
+         
+         <button>
+          FIND WEATHER
+        </button>
+        </nav>
+       <div className="website">
+         <img className="maining" alt=""src="https://via.placeholder.com/140x100"/>
+     
+        
+         <h4>overcast clouds</h4>
+        
+        <p className="p1"> <strong>Temperature </strong>10° to 11°C</p>
+        <p className="p2"><strong>Humidity </strong>78% <strong>Pressure</strong> 1008.48  </p>
+
+         </div>
+         <div className="temp">
+           <div className="img1">
+             <p>03:00</p>
+             <img className="img1-1" alt=""src="https://via.placeholder.com/350x150"/>
+             <p>8°C</p>
+             </div>
+
+
+             <div className="img2">
+             <p>06:00</p>
+             <img className="img2-2" alt=""src="https://via.placeholder.com/140x100"/>
+             <p>9°C</p>
+             </div>
+
+             <div className="img3">
+             <p>09:00</p>
+             <img className="img3-3" alt=""src="https://via.placeholder.com/140x100s"/>
+             <p>14°C</p>
+             </div>
+
+             <div className="img4">
+             <p>12:00</p>
+             <img className="img4-4" alt=""src="https://via.placeholder.com/140x100s"/>
+             <p>17°C</p>
+             </div>
+
+             <div className="img5">
+             <p>15:00</p>
+             <img className="img5-5" alt=""src="https://via.placeholder.com/140x100s"/>
+             <p>18°C</p>
+             </div>
+
+             <div className="img6">
+             <p>18:00</p>
+             <img className="img6-6" alt=""src="https://via.placeholder.com/140x100s"/>
+             <p>16°C</p>
+             </div>
+
+             <div className="img7">
+             <p>21:00</p>
+             <img className="img7-7" alt=""src="https://via.placeholder.com/140x100"/>
+             <p>13°C</p>
+             </div>
+         </div>
       </div>
     );
-  }
-}
+  }}  
 
 export default App;
